ded5c3d26046920af304290facdbab2202ed0a8f
rakibccj <rakibccj@gmail.com>
2022-08-15 16:23:56 +0600

    [PROD-12] Implement UrlConstraint from centralized application endpoint
    -- Cahnge api end point from /product-service/v1/api/product/ to /product-service/api/v1/products/

--- product-service/src/main/java/com/extremecoder/productservice/utils/UrlConstraint.java
+++ product-service/src/main/java/com/extremecoder/productservice/utils/UrlConstraint.java
@@ -5,11 +5,11 @@ public final class UrlConstraint {
     private UrlConstraint() {
     }
 
-    private static final String VERSION = "/v1";
     private static final String API = "/api";
+    private static final String VERSION = "/v1";
 
     public static class ProductManagement {
-        public static final String ROOT = VERSION + API + "/product";
+        public static final String ROOT = API + VERSION + "/products";
         public static final String DELETE = "/{productId}";
         public static final String GET = "/{productId}";
         public static final String PUT = "/{productId}";
