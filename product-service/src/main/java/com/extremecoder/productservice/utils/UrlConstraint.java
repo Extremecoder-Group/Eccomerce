b2cb05c15ffdea8e64798d556768fa9548bdf5d5
rakibccj <rakibccj@gmail.com>
2022-08-15 23:24:14 +0600

    [PROD-12] Implement UrlConstraint from centralized application endpoint
    -- change api version variable VERSION to VERSION1
    -- remove api string and attached directly.

--- product-service/src/main/java/com/extremecoder/productservice/utils/UrlConstraint.java
+++ product-service/src/main/java/com/extremecoder/productservice/utils/UrlConstraint.java
@@ -5,11 +5,10 @@ public final class UrlConstraint {
     private UrlConstraint() {
     }
 
-    private static final String API = "/api";
-    private static final String VERSION = "/v1";
+    private static final String VERSION1 = "/v1";
 
     public static class ProductManagement {
-        public static final String ROOT = API + VERSION + "/products";
+        public static final String ROOT = "/api" + VERSION1 + "/products";
         public static final String DELETE = "/{productId}";
         public static final String GET = "/{productId}";
         public static final String PUT = "/{productId}";
