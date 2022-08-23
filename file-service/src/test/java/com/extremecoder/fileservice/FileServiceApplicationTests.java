50be90fb08c235c65f7048de69499972e7d6eae6
rakibccj <rakibccj@gmail.com>
2022-08-23 22:40:39 +0600

    FileServiceApplicationTests
    -- storeFileTest case

--- file-service/src/test/java/com/extremecoder/fileservice/FileServiceApplicationTests.java
+++ file-service/src/test/java/com/extremecoder/fileservice/FileServiceApplicationTests.java
@@ -1,13 +1,24 @@
 package com.extremecoder.fileservice;
 
+import com.extremecoder.fileservice.service.FileStorageService;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 
 @SpringBootTest
 class FileServiceApplicationTests {
 
+    @Autowired
+    FileStorageService fileStorageService;
+
     @Test
-    void contextLoads() {
+    void storeFileTest() {
+        MockMultipartFile multipartFile =
+                new MockMultipartFile("springboot.png", "springboot.png", "image/png",
+                        "".getBytes());
+
+        fileStorageService.storeFile(multipartFile);
     }
 
 }
