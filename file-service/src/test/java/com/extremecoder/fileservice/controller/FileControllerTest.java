2ba34deb0a89eaf45778f37959d0e2e784a59e32
Md. Badrul Alam <hridoyjbd@gmail.com>
2022-08-29 09:53:40 +0600

    [PROD-17][file-service] FileControllerTest.uploadFile test

--- file-service/src/test/java/com/extremecoder/fileservice/controller/FileControllerTest.java
+++ file-service/src/test/java/com/extremecoder/fileservice/controller/FileControllerTest.java
@@ -1,5 +1,6 @@
 package com.extremecoder.fileservice.controller;
 
+import com.extremecoder.fileservice.model.FileInfo;
 import com.extremecoder.fileservice.service.FileStorageService;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeAll;
@@ -9,39 +10,26 @@ import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.request.MockMultipartHttpServletRequestBuilder;
-import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
-import org.springframework.web.context.WebApplicationContext;
-
-import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.mockito.Mockito.when;
 
 /**
  * File Controller Test
  *
  * @author Badrul
  */
+
 @Slf4j
 @ExtendWith(MockitoExtension.class)
-@AutoConfigureMockMvc
 class FileControllerTest {
 
     @InjectMocks
     FileController fileController;
+
     @Mock
     FileStorageService fileStorageService;
-
-    @Autowired
-    MockMvc mockMvc;
-
 
     @BeforeAll
     static void beforeAll() {
@@ -49,18 +37,12 @@ class FileControllerTest {
     }
 
     @Test
-    @DisplayName("upload single file")
-    void uploadFile() throws Exception {
-//
-//        FileInputStream fis = new FileInputStream("E:\\New folder\\Ancillary Services(1).jpg");
-//
-//        MockMultipartFile multipartFile = new MockMultipartFile("file", fis);
-//
-//        FileInfo fileInfo = fileController.uploadFile(multipartFile);
-
-//        FileInfo fileInfo = fileController.uploadFile(file);
+    @DisplayName("when uploadFile() is called then upload single file")
+    void uploadFile() {
+        log.info("*** FileControllerTest.uploadFile() -- started -- ***");
 
         String fileName = "sampleFile.txt";
+
         MockMultipartFile sampleFile = new MockMultipartFile(
                 "uploaded-file",
                 fileName,
@@ -68,26 +50,22 @@ class FileControllerTest {
                 "This is the file content".getBytes()
         );
 
-        MockMultipartHttpServletRequestBuilder multipartRequest =
-                MockMvcRequestBuilders.multipart("/uploadFile");
+        FileInfo fileInfo = FileInfo.builder()
+                .fileName(fileName)
+                .originalFilename(fileName)
+                .build();
 
-        mockMvc.perform(multipartRequest.file(sampleFile))
-                .andExpect(status().isOk());
-//
-//        Path docRootPath = Path.of(documentRoot, fileName);
-//        filesToBeDeleted.add(docRootPath);
-//        assertThat(Files.exists(docRootPath)).isTrue();
+        log.info("Original fileName: " + fileInfo.getOriginalFilename());
 
-        System.out.println(234);
+        when(fileStorageService.storeFile(sampleFile)).thenReturn(fileInfo);
+
+        FileInfo fileInfoResponse = fileController.uploadFile(sampleFile);
+
+        assertEquals(fileInfo.getOriginalFilename(), fileInfoResponse.getOriginalFilename());
+
+        log.info("*** FileControllerTest.uploadFile() -- ended -- ***");
 
     }
-
-    @Test
-    @DisplayName("Test API")
-    void test() {
-        assertEquals("Hello World!", fileController.test());
-    }
-
 
     @Test
     void uploadMultipleFiles() {
