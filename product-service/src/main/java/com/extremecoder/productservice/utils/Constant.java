7bf0c5c4a694b24ce6d6448158c61cd9218a9615
rakib <rakibccj@gmail.com>
2022-10-11 23:40:49 +0600

    [EE-100][product-service] CategoryList by pagination and sorting.
    -- Get All Categories by pagination and sorting.

---
+++ product-service/src/main/java/com/extremecoder/productservice/utils/Constant.java
@@ -0,0 +1,6 @@
+package com.extremecoder.productservice.utils;
+
+public class Constant {
+    public static final Integer DEFAULT_SEARCH_ITEM = 10;
+    public static final String DEFAULT_SORTED_FIELD = "id";
+}
